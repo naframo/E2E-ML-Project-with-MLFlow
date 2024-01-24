62e7f0d68f4735d48d6d7a2068622430de33fd0d
naframo <11monovation@gmail.com>
2024-01-24 10:29:27 +0700

    exception, logger & utils added

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -111,16 +111,3 @@ def load_bin(path: Path) -> Any:
     return data
 
 
-
-@ensure_annotations
-def get_size(path: Path) -> str:
-    """get size in KB
-
-    Args:
-        path (Path): path of the file
-
-    Returns:
-        str: size in KB
-    """
-    size_in_kb = round(os.path.getsize(path)/1024)
-    return f"~ {size_in_kb} KB"
